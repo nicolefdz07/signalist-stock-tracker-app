7758aea8b8bdf21e4ed3faa4b7e9910ef98ddc6a
Nicole Fernandez Sanchez <n.fernandez17@unapec.edu.do>
2025-11-21 20:45:47 -0400

    implement header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+const Home = () => {
+    return (
+        <div className='flex min-h-screen home-wrapp er'>
+           Home
+        </div>
+
+    )
+}
+export default Home
---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/NavItems";
+import UserDropDown from "@/components/UserDropDown";
+
+const Header = () => {
+    return (
+        <header className='sticky top-0 header'
+        >
+            <div className='container header-wrapper'>
+                <Link href='/'>
+                    <Image src='/assets/icons/logo.svg' alt='signalist logo' width={140} height={32}
+                           className='h-8 w-auto cursor-pointer'/>
+                </Link>
+                <nav className="hidden sm:!block">
+                    <NavItems/>
+                </nav>
+                <UserDropDown/>
+            </div>
+
+        </header>
+    )
+}
+export default Header
